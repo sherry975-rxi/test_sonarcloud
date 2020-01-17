fd77515c12af180446aaa8d971a34077a45e4165
Christopher Wagner <cwwagne2@ncsu.edu>
2020-01-16 23:20:46 -0500

    returned setEmail() to original condition

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -77,7 +77,7 @@ public class Student {
 	public void setEmail(String email) {
 		if((email == null) || (email.isEmpty())
 				|| (!email.contains("@")) || (!email.contains("."))
-				|| (email.indexOf(".") < email.indexOf("@"))
+				|| (email.lastIndexOf(".") < email.indexOf("@"))
 				) {
 			throw new IllegalArgumentException("Invalid email");
 			}
