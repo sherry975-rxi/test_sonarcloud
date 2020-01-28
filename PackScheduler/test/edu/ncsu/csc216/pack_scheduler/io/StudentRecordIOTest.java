94820f6d9e9f4569c4673c3d3dc4f4cbdf4b3166
jrlanois <jrlanois@ncsu.edu>
2020-01-28 09:00:05 -0500

    StudentRecordIOTest Base

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,24 @@
+package edu.ncsu.csc216.pack_scheduler.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentRecordIOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testReadStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+}
