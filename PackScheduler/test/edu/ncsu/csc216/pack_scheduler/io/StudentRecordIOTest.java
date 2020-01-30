7f90eaf02bb303c2b448b1b824daf29174d05c5c
jrlanois <jrlanois@ncsu.edu>
2020-01-30 09:08:12 -0500

    Javadoced StudentRecordIOTest

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -22,6 +22,7 @@ import edu.ncsu.csc216.pack_scheduler.user.Student;
  */
 public class StudentRecordIOTest {
 
+	/** Valid Students*/
 	private String validStudent0 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,pw,15";
 	private String validStudent1 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,pw,4";
 	private String validStudent2 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,pw,14";
@@ -33,15 +34,23 @@ public class StudentRecordIOTest {
 	private String validStudent8 = "Althea,Hicks,ahicks,Phasellus.dapibus@luctusfelis.com,pw,11";
 	private String validStudent9 = "Dylan,Nolan,dnolan,placerat.Cras.dictum@dictum.net,pw,5";
 	
+	/** Array of the valid students that is expected as a result*/
 	private String [] validStudents = {validStudent0, validStudent1, validStudent2, validStudent3, validStudent4, validStudent5,
 	        validStudent6, validStudent7, validStudent8, validStudent9};
 
+	/** Password variable used to check hashed passwords when reading files*/
 	private String hashPW;
+	/** The hashing algorithm used by the program */
 	private static final String HASH_ALGORITHM = "SHA-256";
 	
+	/** A source file of known valid tests*/
 	private static final String VALID_TEST_FILE = "test-files/student_records.txt";
+	/** A source file of known general invalid tests*/
 	private static final String INVALID_TEST_FILE = "test-files/invalid_student_records.txt";
 	
+	/**
+	 * Resets the validStudents ArrayList for use in tests
+	 */
 	@Before
 	public void setUp() {
 	    try {
@@ -58,6 +67,14 @@ public class StudentRecordIOTest {
 	    }
 	}
 	
+	/**
+	 * Method used to check an expected file to see if an file actually made matches the result.
+	 * It fails if the actual file doesn't match the expected file, and succeeds if the files do
+	 * match.
+	 * @param expFile The String for the name of the expected file which is checked against.
+	 * @param actFile The String for the name for the file actually made that is checked to match
+	 * the expected file.
+	 */
 	private void checkFiles(String expFile, String actFile) {
 	    try {
 	        Scanner expScanner = new Scanner(new FileInputStream(expFile));
@@ -82,6 +99,9 @@ public class StudentRecordIOTest {
 	    }
 	}
 
+	/**
+	 * Tests readStudentRecords() with both valid and invalid test files containing general input cases.
+	 */
 	@Test
 	public void testReadStudentRecords() {
 		//Valid Record Test
@@ -104,7 +124,8 @@ public class StudentRecordIOTest {
 	}
 	
 	/**
-	 * Check to see if it can write files correctly
+	 * Checks writeStudentRecords() by checking if it can write to a file, and checking it with the an
+	 * expected result file.
 	 */
 	@Test
 	public void testWriteStudentRecords() {
@@ -119,7 +140,8 @@ public class StudentRecordIOTest {
 	}
 
 	/**
-	 * Checking for a failure in filewriting
+	 * Checks writeStudentRecords() by testing save cases that would make it fail to write files by
+	 * writing to a place where it doesn't have permission to.
 	 */
 	@Test
 	public void testWriteStudentRecordsNoPermissions() {
@@ -137,7 +159,9 @@ public class StudentRecordIOTest {
 	}
 	
 	/**
-	 * Test processing a student without credits defined
+	 * Tests readStudentRecords() with unique inputs to test the different paths in its
+	 * helper method, processStudent(), so as to test for weird cases the program would
+	 * encounter when reading files.
 	 */
 	@Test
 	public void testProcessStudent() {
